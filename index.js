fa4b9effb207ee6124e2d2c99ec1bf6f79be4a9d
Christina Lan <christina.lan13@gmail.com>
2018-11-12 01:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  var newObj = object.assign({},object)
+  var newObj = Object.assign({},object)
   delete newObj[key]
   return newObj
 }
